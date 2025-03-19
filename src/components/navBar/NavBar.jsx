75cd81f0d7940f53d7f60f659329d2a4f53a8d0c
ThiagoVanucci <thiago.dev24@gmail.com>
2025-03-19 16:56:03 -0300

    Mudanças no navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,7 +2,7 @@ import "./NavBar.module.css";
 
 const NavBar = (props) => {
   return (
-    <div className="NavBar">
+    <div className="navbar">
       <img src={props.logo} alt="" />
     </div>
   );
