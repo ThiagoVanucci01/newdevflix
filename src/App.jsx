5e12b6e0796406b77abdb2e3d0fd531826993c85
ThiagoVanucci <thiago.dev24@gmail.com>
2025-03-19 08:57:37 -0300

    Projeto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
