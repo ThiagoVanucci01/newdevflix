57c0d6779981e990c285943c7adb32211b9c9225
ThiagoVanucci <thiago.dev24@gmail.com>
2025-03-19 09:20:07 -0300

    Imagens Adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import Logo from "./assets/devflix.png";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div id="app">
+      <img src={Logo} alt="" />
+    </div>
+  );
 };
 
 export default App;
